495b9ee58b05646aa3153463771ad71a48bd3036
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2025-04-05 17:47:07 -0400

    Add files via upload

---
+++ VigenereCipher.cpp
@@ -0,0 +1,20 @@
+#include "VigenereCipher.h"
+
+/*
+ * definition of the constructor
+ */
+/*TODO: write your code*/
+
+/*
+ * definition of encode()
+ */
+bool VigenereCipher::encode(string msg) {
+  /*TODO: write your code*/
+} 
+
+/*
+ * definition of decode()
+ */
+bool VigenereCipher::decode(string msg) {
+  /*TODO: write your code*/
+}
---
+++ VigenereCipher.h
@@ -0,0 +1,34 @@
+#ifndef VIGENERECIPHER_H
+#define VIGENERECIPHER_H
+
+#include "Cipher.h"
+
+class VigenereCipher : public Cipher {
+private:
+  /*  
+   * original key used in a Vigenere cipher
+   */
+  string key;
+
+public:
+  /* 
+   * constructor: initializes key to the parameter and cipherName to "Vigenere"
+   * @param k - used to set key
+   */
+  VigenereCipher(string k); 
+  
+  /*  
+   * encode(): performs Vigenere encoding of a message
+   * @param msg - string message to encode
+   * @return true if encoding is successful; otherwise false
+   */
+  bool encode(string msg);
+
+  /*  
+   * decode(): performs Vigenere decoding of message
+   * @param msg - string message to decode
+   * @return true if decoding is successful; otherwise false
+   */
+  bool decode(string msg);
+};
+#endif
